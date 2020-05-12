a32d34b51b732c83bd7134ad4dbb93377f8fec33
saltyNaCl <yjiang2001@gmail.com>
2020-05-12 01:19:32 -0400

    perfectly balanced as all things should be

--- bot.py
+++ bot.py
@@ -2,60 +2,53 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='j!')  # create bot with prefix
-
 # read from settings file
 f = open("settings.txt", "r")
 flines = f.readlines()
 for l in flines:
+    l=l.strip()
     if l.startswith("TOKEN"):
         tokenLine = l.split('{', 1)
         TOKEN = tokenLine[1][:-1]
-    else:
-        print('TOKEN not found!')
+    elif l.startswith("PREFIX"):
+        prefixLine = l.split('{', 1)
+        PREFIX = prefixLine[1][:-1]
 
-# load the cogs
-cogStrList = bot.cogs.keys()
-cogcount = 0
-coglist = ''
-for filename in os.listdir('./cogs'):
-    cogName = filename[:-3]  # without the '.py' part
-    if filename.endswith('.py') and cogName not in cogStrList:
-        bot.load_extension(f'cogs.{cogName}')
-        cogcount += 1
-        coglist += filename+' | '
-
+bot = commands.Bot(command_prefix=PREFIX)
 
 @bot.event
 async def on_connect():
     appInfo = await bot.application_info()
-    print(f'Bot Owner: {appInfo.owner.name}#{appInfo.owner.discriminator}')
+    print(f'Bot Owner: {appInfo.owner.name}#{appInfo.owner.discriminator}  |  Bot Prefix: {PREFIX}')
     print('Successfully connected to Discord...')
 
-# @bot.event
-# async def on_disconnect():
-#     cogStrList = bot.cogs.keys()
-#     #unload the cogs to prevent error
-#     for cog in cogStrList:
-#         bot.unload_extension(f'cogs.{cog}')
+    # load the cogs
+    cogStrList = bot.cogs.keys()
+    cogcount = 0
+    coglist = ''
+    for filename in os.listdir('./cogs'):
+        cogName = filename[:-3]  # without the '.py' part
+        if filename.endswith('.py') and cogName not in cogStrList:
+            bot.load_extension(f'cogs.{cogName}')
+            cogcount += 1
+            coglist += filename+' | '
 
+    print(f'Loaded {cogcount} cogs: {coglist[:-3]}')
 
 @bot.event
 async def on_ready():
-    # print the logic from line 17
-    print(f'Loaded {cogcount} cogs: {coglist[:-3]}')
 
     membercount = 0
     for member in bot.get_all_members():
         membercount += 1
-    print(f'{membercount} members total')
 
     serverlist = ''
     guilds = await bot.fetch_guilds().flatten()
     for guild in guilds:
         serverlist += guild.name+' | '
-    print(f'In {len(guilds)} servers: {serverlist[:-3]}')
 
+    print(f'{membercount} members total')
+    print(f'In {len(guilds)} servers: {serverlist[:-3]}')
     print('\nBOT IS READY!')
 
 bot.run(TOKEN)
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 type ThanosBotAscii.txt
-python bot.py
+py bot.py
