1deed14cc3eddcc4f665b067fa1552727cca2b3f
saltyNaCl <yjiang2001@gmail.com>
2020-05-12 05:42:48 -0400

    prevent hoisting nicknames

--- cogs/util.py
+++ cogs/util.py
@@ -12,6 +12,36 @@ class Util(commands.Cog):
     async def ping(self,ctx):
         """| pong! returns latency"""
         await ctx.send(f'Pong! {round(self.bot.latency*1000)}ms')
+    
+    @commands.command(aliases=['noAttention','removeNicks'])
+    @commands.is_owner()
+    async def removeNick(self,ctx,usercount=10,symbol='!',*,newNick=None):
+        """| remove/change nicknames that start with \"symbol\""""
+        if ctx.guild == None:
+            await ctx.send(f'Hey! You need to type that command in a server!')
+        else:
+            badnamelist = []
+            for member in ctx.guild.members:
+                if member.display_name.startswith(symbol):
+                    badnamelist.append(member)
+
+            badnamecount=len(badnamelist) #number of bad names found in total
+            await ctx.send(f'Attempting to change {usercount} names that start with `{symbol}` ...')
+
+            changecount = 0             
+            if badnamecount <= usercount:
+                for member in badnamelist:
+                    await member.edit(nick=newNick)
+                    changecount += 1
+                await ctx.send(
+                    f'Only {badnamecount} bad nicknames found. {changecount} members changed to `{newNick}`')
+            else: # since there are too many bad names, only change the specified amount 
+                for member in badnamelist:
+                    if changecount < usercount:
+                        await member.edit(nick=newNick)
+                        changecount += 1
+                await ctx.send(
+                    f'{badnamecount} bad nicknames found. {changecount} members changed to `{newNick}`')
 
 def setup(bot):
     bot.add_cog(Util(bot))
