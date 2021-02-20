c92a12fadbfbb2c36396c212cda1f92611f96ab9
jiango <yjiang2001@gmail.com>
2021-02-20 18:25:58 -0500

    +txt

--- cogs/AntiSlur.py
+++ cogs/AntiSlur.py
@@ -1,18 +1,33 @@
 import discord
 import asyncio
 from discord.ext import commands
+from pathlib import Path
 
 # NO COMMANDS HERE, only 1 listener for on_message()
 # Designed only for Heavenly Realm
+
+
 class AntiSlur(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
+        # fetch file path
+        fPath = Path(__file__).parents[1].joinpath(
+            "data", "slursFilter.txt")
+        wPath = Path(__file__).parents[1].joinpath(
+            "data", "wildcardFilter.txt")
+
+        with open(fPath) as f:
+            self.slursList = f.readlines()
+        
+        with open(wPath) as w:
+            self.wildcardList = w.readlines()
+
     @commands.Cog.listener()
     async def on_message(self, msg):
-        msgContent = msg.content.lower()
-        trigger = msgContent.split()
+        msgContentLower = msg.content.lower()
+        trigger = msgContentLower.split()
 
         # if msg was sent in DM (not in a guild)
         if msg.guild == None:
@@ -33,30 +48,40 @@ class AntiSlur(commands.Cog):
             return
 
         # Racial slur filter
-        if ("nigger" in msgContent or "chink" in msgContent or "kike" in trigger or "spic" in trigger
-                or "gook" in trigger or "honkey" in trigger):
+        # standalone word match (not wildcard match)
+        for s in self.slursList:
+    
+            if s in trigger:
+                muted = msg.guild.get_role(316401466875314178)
+                modrole = msg.guild.get_role(388736972845482004)
 
-            muted = msg.guild.get_role(316401466875314178)
-            modrole = msg.guild.get_role(388736972845482004)
-
-            try:
-                if modrole in authorRoles:
+                try:
+                    if modrole in authorRoles:
+                        await msg.delete()
+                        return
+                    else:
+                        await msg.delete()
+                        await msg.author.add_roles(muted, reason='racial slur')
+                        await msg.channel.send(f'{msg.author.mention} has been {muted.mention} (for 69 mins) '
+                                               + '<a:aComicSans:528411471990751235>')
+                        await msg.channel.send("**SO GUYS WE DID IT WE ENDED RACISM**")
+                        modlog = self.bot.get_channel(316332561448042496)
+                        logmsg = await modlog.send(msg.author.mention + " was temporarily muted for racial slur")
+                        await asyncio.sleep(4140)
+                        await msg.author.remove_roles(muted, reason='timed mute is over')
+                        await logmsg.edit(content=f'{msg.author.mention} was ~~temporarily muted for racial slur~~ unmuted')
+                        return
+                except discord.HTTPException:
+                    pass
+        
+        # wildcard match
+        for w in self.wildcardList:
+            if w in msgContentLower:
+                try:
                     await msg.delete()
                     return
-                else:
-                    await msg.delete()
-                    await msg.author.add_roles(muted, reason='racial slur')
-                    await msg.channel.send(f'{msg.author.mention} has been {muted.mention} (for 69 mins) '
-                                        + '<a:aComicSans:528411471990751235>')
-                    await msg.channel.send("**SO GUYS WE DID IT WE ENDED RACISM**")
-                    modlog = self.bot.get_channel(316332561448042496)
-                    logmsg = await modlog.send(msg.author.mention + " was temporarily muted for racial slur")
-                    await asyncio.sleep(4140)
-                    await msg.author.remove_roles(muted, reason='timed mute is over')
-                    await logmsg.edit(content=f'{msg.author.mention} was ~~temporarily muted for racial slur~~ unmuted')
-                    return
-            except discord.HTTPException:
-                pass
+                except discord.HTTPException:
+                    pass
 
 
 def setup(bot):
